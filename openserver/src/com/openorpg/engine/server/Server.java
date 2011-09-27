490c55e2f98336ae029b482ce511154d979a9f89
Raymond Cox <Raymond Cox@rjcomputer.transcendata.com>
2011-09-27 01:08:10 -0400

    initial commit

---
+++ openserver/src/com/openorpg/engine/server/Server.java
@@ -0,0 +1,14 @@
+package com.openorpg.engine.server;
+import java.util.logging.Logger;
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Logger logger = Logger.getLogger(Server.class.getName());
+		logger.info("Hello server!");
+	}
+
+}
